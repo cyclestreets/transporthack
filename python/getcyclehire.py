0d6214495f4189680bd2ab7f316a6103927f98c2
Martin Dunschen <mdunschen@gmail.com>
2016-03-12 15:27:15 +0000

    Added to repo, use of bbox, updated data

--- python/getcyclehire.py
+++ python/getcyclehire.py
@@ -5,13 +5,10 @@ import json
 boxjsonurl = "https://raw.githubusercontent.com/cyclestreets/transporthack/master/geodata/ld-local-authority-bbox.geojson"
 boxjson = requests.get(boxjsonurl).json()
 box = boxjson["features"][0]["geometry"]["coordinates"]
-print len(box[0])
-print len(box[0][0])
 c0 = box[0][0][0], box[0][0][1]
 c1 = box[0][1][0], box[0][1][1]
 c2 = box[0][2][0], box[0][2][1]
 c3 = box[0][3][0], box[0][3][1]
-print c0, c1, c2, c3
 ll = min(c0[0], c1[0], c2[0], c3[0]), min(c0[1], c1[1], c2[1], c3[1])
 ur = max(c0[0], c1[0], c2[0], c3[0]), max(c0[1], c1[1], c2[1], c3[1])
 
@@ -31,10 +28,7 @@ r = requests.get(url)
 # search json for any bike rental tags
 
 j = r.json()
-print type(j)
 allfeatures = j["features"]
-
-print len(allfeatures)
 
 # hack, bike shops with hire
 hackedhire = {
@@ -70,5 +64,5 @@ for f in allfeatures:
 
 
 d = json.dumps(j)
-open("outputcyclehireshops.json", "w").write(d)
+open("../data/outputcyclehireshops.json", "w").write(d)
 
