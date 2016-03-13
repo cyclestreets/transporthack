1b1a50fd7d82933c3a93494312c18b4166359c97
Martin Dunschen <mdunschen@gmail.com>
2016-03-13 10:28:15 +0000

    tidied code for reuse

--- python/getbicyclerentalOSM.py
+++ python/getbicyclerentalOSM.py
@@ -5,43 +5,53 @@ import json
 import overpy
 import geojson
 
-boxjsonurl = "https://raw.githubusercontent.com/cyclestreets/transporthack/master/geodata/ld-local-authority-bbox.geojson"
-boxjson = requests.get(boxjsonurl).json()
-box = boxjson["features"][0]["geometry"]["coordinates"]
-c0 = box[0][0][0], box[0][0][1]
-c1 = box[0][1][0], box[0][1][1]
-c2 = box[0][2][0], box[0][2][1]
-c3 = box[0][3][0], box[0][3][1]
-ll = min(c0[0], c1[0], c2[0], c3[0]), min(c0[1], c1[1], c2[1], c3[1])
-ur = max(c0[0], c1[0], c2[0], c3[0]), max(c0[1], c1[1], c2[1], c3[1])
+
+def getbbox():
+    boxjsonurl = "https://raw.githubusercontent.com/cyclestreets/transporthack/master/geodata/ld-local-authority-bbox.geojson"
+    boxjson = requests.get(boxjsonurl).json()
+    box = boxjson["features"][0]["geometry"]["coordinates"]
+    c0 = box[0][0][0], box[0][0][1]
+    c1 = box[0][1][0], box[0][1][1]
+    c2 = box[0][2][0], box[0][2][1]
+    c3 = box[0][3][0], box[0][3][1]
+    ll = min(c0[0], c1[0], c2[0], c3[0]), min(c0[1], c1[1], c2[1], c3[1])
+    ur = max(c0[0], c1[0], c2[0], c3[0]), max(c0[1], c1[1], c2[1], c3[1])
 
 
-# OSM overpass API wants the bounding box like this:
-bbox = (ll[1], ll[0], ur[1], ur[0])
+    # OSM overpass API wants the bounding box like this:
+    bbox = (ll[1], ll[0], ur[1], ur[0])
+    return bbox
 
 
-api = overpy.Overpass()
+def queryOSM(qstr):
+    api = overpy.Overpass()
 
-# fetch all ways and nodes
-r = api.query("""
-[out:json][timeout:25];
-(
-  node["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
-  way["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
-  relation["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
-);
-out;
-    """)
+    # fetch all ways and nodes
+    return api.query(qstr)
 
-'''
-"properties": {"website": null, "osmTags": null, "id": "101374", "name": "Kirkby Stephen Cycle Centre"}
-'''
+def overpass2geojson(r):
+    features = []
+    for n in r.nodes:
+        p = geojson.Point((n.lon, n.lat))
+        f = geojson.Feature(geometry=p, properties={'osmTags':n.tags, 'website':"website" in n.tags and n.tags["website"] or None, 'name':n.tags["name"]})
+        features.append(f)
 
-features = []
-for n in r.nodes:
-    p = geojson.Point((n.lon, n.lat))
-    f = geojson.Feature(geometry=p, properties={'osmTags':n.tags, 'website':"website" in n.tags and n.tags["website"] or None, 'name':n.tags["name"]})
-    features.append(f)
+    fl = geojson.FeatureCollection(features)
+    return fl
 
-fl = geojson.FeatureCollection(features)
+bbox = getbbox()
+qstr = """
+    [out:json][timeout:25];
+    (
+      node["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
+      way["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
+      relation["amenity"="bicycle_rental"](%f,%f,%f,%f);""" % bbox + """
+    );
+    out;
+"""
+
+r = queryOSM(qstr)
+
+fl = overpass2geojson(r)
+
 open("../geodata/bicyclehirefromOSM.geojson", "w").write(geojson.dumps(fl))
