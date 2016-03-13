e0b37f4ee44b51cc8d0f6d3e990e28f6b3434643
Matthew Whittle <gy14mw@leeds.ac.uk>
2016-03-13 15:36:50 +0000

    leaflet map with core network

--- public_html/leaflet/data/json_railwaystations6.js
+++ public_html/leaflet/data/json_railwaystations6.js
@@ -1,4 +1,4 @@
-var json_railwaystations6={
+var json_RailwayStations6={
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
