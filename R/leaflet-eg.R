87269eb0a512619e0ca484d3bed0a9f9f315d979
robinlovelace <rob00x@gmail.com>
2016-03-15 10:51:51 +0000

    Update leaflet example

--- R/leaflet-eg.R
+++ R/leaflet-eg.R
@@ -1,8 +1,12 @@
 library(leaflet)
 
+cents = geojson_read("geodata/top-7-arrivals.geojson", what = "sp")
+
 m = leaflet() %>%
   addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>% 
-  addPolygons(data = ld, fillColor = "red")
+  addPolygons(data = ld, fillColor = "red") %>% 
+  # addMarkers(data = cents)
+  addCircleMarkers(data = cents, radius = cents$Arrivals / mean(cents$Arrivals)* 3)
 
 old = setwd("public_html/") # switch working directory
 htmlwidgets::saveWidget(m, selfcontained = T, file = "map-outline.html")
