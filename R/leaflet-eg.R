36c91c040b0cbc69e5c5ca2d60e4be2001dcd018
mwhittle91 <gy14mw@leeds.ac.uk>
2016-03-15 11:30:20 +0000

    added function to leaflet R map

--- R/leaflet-eg.R
+++ R/leaflet-eg.R
@@ -1,12 +1,23 @@
 library(leaflet)
+library(geojsonio)
 
-cents = geojson_read("geodata/top-7-arrivals.geojson", what = "sp")
+arrivals = geojson_read("geodata/top-7-arrivals.geojson", what = "sp")
+boundary = geojson_read("geodata/ld-local-authority.geojson", what = "sp")
+core_net = geojson_read("geodata/rnet_lakes.geojson", what = "sp")
 
 m = leaflet() %>%
   addTiles(urlTemplate = "http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>% 
-  addPolygons(data = ld, fillColor = "red") %>% 
-  # addMarkers(data = cents)
-  addCircleMarkers(data = cents, radius = cents$Arrivals / mean(cents$Arrivals)* 3)
+  addPolylines(data = boundary, group = "Boundary")%>%
+  addPolylines(data = core_net, weight = core_net$n, color = "red", opacity = 0.3, group = "network" )%>%
+  addCircleMarkers(data = arrivals, radius = arrivals$Arrivals / mean(arrivals$Arrivals)* 3)%>%
+  hideGroup ("Boundary")%>%
+  addLayersControl(
+      overlayGroups = c("network", "Boundary"),
+      options = layersControlOptions(collapsed = FALSE)
+  )
+m
+
+
 
 old = setwd("public_html/") # switch working directory
 htmlwidgets::saveWidget(m, selfcontained = T, file = "map-outline.html")
